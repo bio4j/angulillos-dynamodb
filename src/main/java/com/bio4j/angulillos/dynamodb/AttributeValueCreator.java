fc0a7d994c14912bb2417d25f0bbe591197f5a32
balberski <alberskib@gmail.com>
2015-02-01 20:58:58 +0100

    [#1] Remove redundant code

--- src/main/java/com/bio4j/angulillos/dynamodb/AttributeValueCreator.java
+++
@@ -1,9 +0,0 @@
-package com.bio4j.angulillos.dynamodb;
-
-
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-
-public interface AttributeValueCreator<T extends Object> {
-
-    AttributeValue create(T value);
-}
